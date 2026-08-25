3d3e2ebea1733833a4f9f3efe86116cdef66fbe8
EnDoZaKi <38246228+EnDoZaKi@users.noreply.github.com>
2026-08-25 07:41:24 +0000

    Deploying to gh-pages from @ ptozax/EFT_TaskTrack@6e7f1b4696776eaeced9227e60b4829ec3a7546e 🚀

--- assets/optimizerCore-CAebYC2A.js
+++ assets/optimizerCore-CAebYC2A.js
@@ -1,4 +1,4 @@
-import{r as cr,j as R,R as Fs}from"./index-B3MyaGsz.js";/*!
+import{r as cr,j as R,R as Fs}from"./index-BnkvjjAH.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
