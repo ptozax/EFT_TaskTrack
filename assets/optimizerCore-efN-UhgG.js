875c6b84db90a4078522e618f0a38ac603ee8984
EnDoZaKi <EnDoZaKi@users.noreply.github.com>
2026-09-01 20:48:59 +0000

    Deploying to gh-pages from @ ptozax/EFT_TaskTrack@7f3c91b7a84328fb74c9bcb6d4f977756363fbec 🚀

--- assets/optimizerCore-efN-UhgG.js
+++ assets/optimizerCore-efN-UhgG.js
@@ -1,4 +1,4 @@
-import{r as cr,j as R,R as Fs}from"./index-WiAd4414.js";/*!
+import{r as cr,j as R,R as Fs}from"./index-CXmFaAxx.js";/*!
  * html2canvas 1.4.1 <https://html2canvas.hertzen.com>
  * Copyright (c) 2022 Niklas von Hertzen <https://hertzen.com>
  * Released under MIT License
